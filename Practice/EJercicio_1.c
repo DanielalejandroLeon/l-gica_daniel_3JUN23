ea1492190b0b03b040158f0c7be573870ade7f57
DanielalejandroLaon <dibujantecreador@gmail.com>
2023-06-17 18:12:51 -0400

    Finalizó ejercicio 2

---
+++ Practice/EJercicio_1.c
@@ -0,0 +1,28 @@
+/*
+1. Hacer un función que diga si un número es primo
+*/
+#include <stdio.h>
+int main () {
+  int num;
+  int count = 0;
+
+  printf("ingrese un numero:");
+  scanf("%d", &num);
+
+  for (int i =num; i > 0; i--) {
+    if (num % i == 0) {
+      count++;
+    }
+  }
+
+  if (count == 2) {
+
+    printf("El numero %d es primo", num);
+  } else {
+
+    printf("El numero %d no es primo", num);
+
+  }
+  return 0;
+
+}
--- sintax_execirses/execirse_5.c
+++ sintax_execirses/execirse_5.c
@@ -31,8 +31,120 @@ Donde tipo es uno de los tipos de variables de C (como int) y
 nombre variable es el nombre de la variable (como X minombre). El signo igual se utiliza para asignar un valor a un variable
 
    */
-  // crear una virable que almacene un número entero
+  // crear una virable que almacene un número entero 
 
-  int miNumero = 15;
+  /*Tambien puede asignar una variable sin asignar el valor y colocarselo más tarde
+  
+  Declaración de la variable 
+  myNum = 15;  
+  */
+
+  /*
+  si intentamos imprimir el valor de una variable en C no va a ocurrir nada si no le damos el formato específico*/
+/*
+  int miNum = 15;
+
+  printf (myNum); //nada va a ocurrir
+*/
+  /*
+  Para imprimir o hacer uso de las variables en C debemos familiarizarnos con los especificadores de formato
+  */
+
+//especificadores de formato
+
+/*
+Los espesificadores de formato se usan con la función sprinft () para decirle al compilador que tipo de datos está almacenando la variable
+
+Un espesificador de formato comienza con un signo de %, seguido de un caracter.
+
+Por ejemplo, para mantener el valor de una variable int debe usar el espicificador de formato %d o %i y estar entre comillas dobles, dentro de la funcion printf()
+
+*/
+
+/*
+int miNumero = 20;
+printf ("id", miNumero) //la salida es 20
+*/
+//Para imprimir otros tipos, use %c para char y %f para float
+
+//decimal o de coma
+/*
+char miletra = 'N'
+float minumerodecimal = 9.33;
+//impresión de variables 
+
+printf("%f\n",minumerodecimal);
+printf("%c\n",miletra);
+*/
+
+/*
+Para combinar texto y una variable, separalos con una coma dentro dela función printf
+*/
+/*
+int main ()
+{
+int miNumero =50;
+
+printf ("mi numero favorito es el: %d", miNumero);
+*/
+ /*
+ Para imprimir diferentes tipos de varibles en un solo printf() se puede hacer lo siguiente: 
+ */
+//ccambiar valores de variables 
+
+/*
+Si asignamos un nuevo valor a una variable existente, sobre escribiremos el valor anterior
+*/
+/*
+int miNumero = 19;
+
+miNumero = 91;
+
+printf("%d", miNumero);
+
+*/
+
+/*
+Tambien podemos asignar el valor de una variable a otra
+*/
+
+/*
+int main()
+{
+int miNUmero = 57;
+
+int miOtroNúmero = 14;
+
+//asignamos el valor de miOtro Número (14) a miNumero
+
+miNUmero = miOtroNúmero;
+//miNumero es ahora el 14, en lugar del 47
+
+printf("%d", miNUmero);*/
+/*
+int main()
+{
+int minumero = 57;
+
+//Declaramos una variable sin valor
+int miOtroNumero;
+
+
+miOtroNumero = minumero;
+printf ("%d", miOtroNumero);
+*/
+//sumar valores de variables
+int main ()
+{
+ int y = 8;
+ int x = 15;
+ int suma = x + y;
+
+printf ("%d", suma);
+
+}
+
+
+
  return 0;
 }
